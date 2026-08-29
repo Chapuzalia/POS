a582d05aec59a459e28f8ca8268857bcb98943ed
GerardNadal <gnadalar@gmail.com>
2026-08-29 23:20:26 +0200

    feat: add migration script for correcting print agent pairing code

---
+++ supabase/migrations/20260829220000_correct_codigo_produccion.sql
@@ -0,0 +1,5 @@
+ALTER FUNCTION public.create_print_agent_pairing_code(uuid)
+SET search_path = pg_catalog, extensions;
+
+ALTER FUNCTION public.unlink_print_agent(uuid)
+SET search_path = pg_catalog, extensions;
